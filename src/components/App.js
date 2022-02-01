2b88f5aed5e98f64a6e75acc41bd1b502581ee3b
Jan Richter <j4n.ric@gmail.com>
2022-02-01 14:29:49 +0100

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import StreamDelete from "./streams/StreamDelete";
+
+import Header from "./Header";
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<StreamList />} />
+          <Route path="/streams/new" element={<StreamCreate />} />
+          <Route path="/streams/edit" element={<StreamEdit />} />
+          <Route path="/streams/delete" element={<StreamDelete />} />
+          <Route path="/streams/show" element={<StreamShow />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
