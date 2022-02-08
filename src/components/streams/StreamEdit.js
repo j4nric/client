56d053238a6df43ae8f69ee5d0aed7d9c3d2d0d3
Jan Richter <j4n.ric@gmail.com>
2022-02-08 23:51:51 +0100

    Fixed StreamEdit to receive streams data

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,18 +1,48 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import { connect } from "react-redux";
+import React, {useEffect} from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
+import { useParams } from 'react-router-dom';
 
-const StreamEdit = (props) => {
-  const params = useParams();
-  return (
-    <div>
-      Stream Edit Page: <b>{params.id}</b>
-    </div>
-  );
+export function withRouter(){
+
+  return(props)=>{
+
+    console.log(props);
+     const match  = {params: useParams()};
+     return <StreamEdit {...props}  match = {match}/>
+ }
 };
 
+const StreamEdit = (props) => {
+
+  console.log("props inside stream edit", props);
+
+  useEffect(() => {
+    props.fetchStream(props.match.params.id);
+  }, []);
+
+
+    //console.log(props.stream);
+    if (!props.streams || !Object.keys(props.streams).includes(props.match.params.id)) {
+      return <div>Loading...</div>;
+    }
+
+    return <div>{props.streams[props.match.params.id].title}</div>;
+  
+}
+
 const mapStateToProps = (state, ownProps) => {
+
+  console.log(ownProps);
+  // console.log(state.streams[1]);
+  // state.streams[ownProps.match.params.id]
+  return { streams: state.streams }
+
+
   return { stream: null };
 };
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(withRouter(StreamEdit));
